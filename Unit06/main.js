ed6d1c133d199912e4034d5641b4bcab54ca8f46
Wyatt <wyatthunterallan@gmail.com>
2019-09-30 17:43:20 -0600

    Got the button to work by removing the parenthesis and it does diplay the information in the p column! Just need to get the input from the user to fill in the blanks!

---
+++ Unit06/main.js
@@ -0,0 +1,24 @@
+
+let nounString = document.querySelector('#nounTextArea').value
+console.log(nounString)
+let adjectiveString = document.querySelector('#adjectiveTextArea').value
+let verbString = document.querySelector('#verbTextArea').value
+
+
+function tellStory() {
+    nounString.toLowerCase()
+    nounArray = nounString.split(/\s+/)
+    adjectiveString.toLowerCase()
+    adjectiveArray = adjectiveString.split(/\s+/)
+    verbString.toLowerCase()
+    verbArray = verbString.split(/\s+/)
+ 
+    let storyArea = `Once upon a time ${nounArray[0]} slapped test 
+    cuz the ${adjectiveArray[0]} man was in his way so they ${verbArray[0]} him`;
+    document.querySelector('#storyP').textContent = storyArea
+
+}
+
+document.querySelector('#storyButton').onclick = tellStory
+
+console.log()
