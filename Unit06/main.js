a724611559b933abc6c9aa4d88c579f68d64a2cd
Wyatt <wyatthunterallan@gmail.com>
2019-10-01 11:18:39 -0600

    Updates to the story and styling in CSS

--- Unit06/main.js
+++ Unit06/main.js
@@ -17,8 +17,15 @@ function tellStory() {
     verbString.toLowerCase()
     verbArray = verbString.split(/\s+/)
  
-    let storyArea = `Once upon a time ${nounArray[0]} slapped test 
-    cuz the ${adjectiveArray[0]} man was in his way so they ${verbArray[0]} him`;
+    let storyArea = `Once upon a time, the mighty travelers ${nounArray[0]} and ${nounArray[1]} were
+    summoned to the ${adjectiveArray[0]} castle but, alas, they were stopped in their
+    tracks by ${nounArray[2]}. ${nounArray[2]} tried to ${verbArray[0]} ${nounArray[0]}
+    and ${nounArray[1]} but it didn't work. So, instead they began to call ${nounArray[2]}
+    a ${adjectiveArray[1]} loser! ${nounArray[2]} didn't like that very much so he put his
+    hand in his ${adjectiveArray[2]} pocket and blew on his flute. ${nounArray[3]}, ${nounArray[4]}
+    and ${nounArray[5]} all came running to his aid. "Stop!" they said! "Or else we will
+    report you to King ${nounArray[6]}! ${nounArray[0]} and ${nounArray[1]} started to
+    ${verbArray[1]} but it did not work. They were beheaded by King ${nounArray[6]}.`
     
     document.querySelector('#storyP').textContent = storyArea
 
