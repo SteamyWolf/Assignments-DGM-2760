b3cbae30e991e03e663bcdd7844bf01a17e7de78
Wyatt <wyatthunterallan@gmail.com>
2019-10-07 17:36:00 -0600

    The Build button is working. Just gotta work on Shopping button

---
+++ Unit07/main.js
@@ -0,0 +1,35 @@
+let message
+
+const pizza = {
+    crust: 'thin',
+    size: 'large',
+    topping: 'pepperoni',
+    buildPizza: function () {
+        console.log('buildPizza method has been called')
+        message = `Baking a ${pizza.size} Pizza with ${pizza.crust} crust with ${pizza.topping} toppings`
+        document.querySelector('#feedback').textContent = message
+    },
+    shoppingList: () => {
+        let flour 
+        if (pizza.crust === 'thick') flour *= 2
+        // do the same for large property
+        message = `You will need to have ${flour} cups of flour and 1 lb of ${pizza.topping}.`
+        document.querySelector('#feedback').textContent = message
+    }
+}
+
+document.querySelector('#thin').addEventListener('click', () => pizza.crust = 'thin')
+document.querySelector('#thick').addEventListener('click', () => pizza.crust = 'thick')
+
+document.querySelector('#sausage').addEventListener('click', () => pizza.topping = 'sausage') 
+document.querySelector('#pepperoni').addEventListener('click', () => pizza.topping = 'pepperoni')
+
+document.querySelector('#small').addEventListener('click', () => pizza.size = 'small')
+document.querySelector('#large').addEventListener('click', () => pizza.size = 'large')
+
+
+
+
+
+document.querySelector('#build').addEventListener('click', pizza.buildPizza)
+document.querySelector('#shopping').addEventListener('click', pizza.shoppingList)
